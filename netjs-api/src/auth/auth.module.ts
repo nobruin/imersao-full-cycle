332e434aebeabee0440af62660e5b038622f6087
nobruin <nobruin@gmail.com>
2021-09-16 00:31:46 -0300

    second day

--- netjs-api/src/auth/auth.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { JwtStrategyService } from './jwt-strategy/jwt-strategy.service';
-
-@Module({
-  providers: [JwtStrategyService],
-})
-export class AuthModule {}
